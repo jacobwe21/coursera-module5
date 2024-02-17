bfdb27faf743604e52d7f02ef242a8ba2c05caf5
Jacob Esselstyn <jacobwe21@gmail.com>
2024-02-17 12:47:50 -0500

    v1.2

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [buildAndShowHomeHTML(allCategoriesUrl)]
+  buildAndShowHomeHTML(allCategoriesUrl),
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
