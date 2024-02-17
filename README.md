5c6010a797a5431c6e78a98a8d768d4fcf81d1fb
Jacob Esselstyn <jacobwe21@gmail.com>
2024-02-17 12:21:30 -0500

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# coursera-module5
+This repository is part of a Coursera course for HTML, CSS, and JS. 
+The module 5 solution is on the linked GitHub pages site.
--- js/script.js
+++ js/script.js
@@ -63,11 +63,7 @@ var switchMenuToActive = function () {
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
-// TODO: STEP 0: Look over the code from
-// *** start ***
-// to
-// *** finish ***
-// below.
+// TODO: STEP 0: Look over the code from ***start*** to ***finish*** below.
 // We changed this code to retrieve all categories from the server instead of
 // simply requesting home HTML snippet. We now also have another function
 // called buildAndShowHomeHTML that will receive all the categories from the server
@@ -83,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  [buildAndShowHomeHTML(allCategoriesUrl)]
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -99,30 +95,19 @@ function buildAndShowHomeHTML (categories) {
     function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-      // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName variable's name implies it expects.
+      var chosenCategoryShortName = chooseRandomCategory(categories).catShortName;
 
-
-      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
+      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
-      // WARNING! You are inserting something that will have to result in a valid Javascript
-      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-      // to look like. For example, a valid call would look something like this:
+      // WARNING! You are inserting something that will have to result in a valid Javascript syntax because the substitution of {{randomCategoryShortName}} becomes an argument being passed into the $dc.loadMenuItems function. Think about what that argument needs to look like. For example, a valid call would look something like this:
       // $dc.loadMenuItems('L')
-      // Hint: you need to surround the chosen category short name with something before inserting
-      // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
-
+      // Hint: you need to surround the chosen category short name with something before inserting it into the home html snippet.
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,randomCategoryShortName,chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
-      // Use the existing insertHtml function for that purpose. Look through this code for an example
-      // of how to do that.
-      // ....
-
+      // Use the existing insertHtml function for that purpose. Look through this code for an example of how to do that.
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
